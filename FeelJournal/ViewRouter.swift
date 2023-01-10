0558d2424af665bf519b8e59f58082e2e0d4c148
kevin.jonathan <kevin.jonathan@gdn-commerce.com>
2023-01-10 19:16:51 +0700

    Add Unit test target, update readme, add documentation for ViewRouter

--- FeelJournal/ViewRouter.swift
+++ FeelJournal/ViewRouter.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+
+/// Used to bridge between views and the router
 struct ViewRouter<T: Hashable, Content: View>: View {
     @ObservedObject var router: Router<T>
     @ViewBuilder var buildView: (T) -> Content
@@ -22,12 +24,14 @@ struct ViewRouter<T: Hashable, Content: View>: View {
     }
 }
 
+/// Router paths
 enum Path: Hashable {
     case root
     case addJournal
     case journalDetail(String)
 }
 
+/// Router class with many utilities
 final class Router<T: Hashable>: ObservableObject {
     @Published var root: T
     @Published var paths: [T] = []
@@ -35,19 +39,25 @@ final class Router<T: Hashable>: ObservableObject {
     init(root: T) {
         self.root = root
     }
-
+    
+    /// Pushes a new screen
+    /// - Parameter path: Router path
     func push(_ path: T) {
         paths.append(path)
     }
-
+    
+    /// Pop from current screen, go back to previous screen
     func pop() {
         paths.removeLast()
     }
-
+    
+    /// Update the current screen root
+    /// - Parameter root: screen path
     func updateRoot(root: T) {
         self.root = root
     }
-
+    
+    /// Quickly pop from current screen to the root screen
     func popToRoot() {
         paths = []
     }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # iOS-FeelJournal
-A native iOS app that help users in journaling and capture their daily feeling using Natural Language Processing (NLP).
+FeelJournal is a journaling app designed to track your feeling using NLP (especially Sentiment Analysis)!
+This app can detect whether you have a Happy, Neutral, or Sad feeling on each journal entries.
+
+This app has several features:
+1. Simply write your journal to detect your feeling
+2. Edit your existing journals
+3. Search journal by title or content
+4. Check your overall feelings in Analytics page
